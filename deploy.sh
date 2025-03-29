8607983e9323a5a920e07df0912a1dcee97fcc63
Vitaliy Boyarsky <boyarsky.vitaliy@live.com>
2025-03-29 23:57:21 +0200

    Uninstall openssl from lambda

--- deploy.sh
+++ deploy.sh
@@ -3,21 +3,35 @@
 set -e
 cd $(dirname $0)
 
-if [[ $# -ne 2 ]] && [[ $# -ne 3 ]] ;  then
-  echo "Usage: $0 <name> <bucket> <profile>"
+if [[ $# -ne 3 ]] && [[ $# -ne 4 ]] ;  then
+  echo "Usage: $0 <name> <bucket> <python> <profile>"
   echo ''
   echo 'Options:'
   echo '  name           Name used as prefix for resources'
   echo '  bucket         S3 bucket name used to store source code'
+  echo '  python         Python version to use'
   echo '  profile        Use a specific AWS profile from your credential file'
   exit 1
 fi
 
 NAME=$1
 BUCKET=$2
-if [[ $# -eq 3 ]] ; then
-  export AWS_PROFILE=$3
+PYTHON_VERSION=$3
+if [[ $# -eq 4 ]] ; then
+  export AWS_PROFILE=$4
 fi
+
+echo 'Packaging requirements...'
+rm -rf target
+
+pip3 install -r requirements.txt \
+  --platform=manylinux2014_x86_64 \
+  --python-version $PYTHON_VERSION \
+  --only-binary :all: \
+  --implementation cp \
+  --target target/python/lib/python$PYTHON_VERSION/site-packages
+
+(cd target && zip -r requirements.zip ./python)
 
 trap "rm -f packaged-stack.yaml" EXIT
 
